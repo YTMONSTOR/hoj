635243a4f29fed0b5571e0ca44e4b6d7d6926444
YTMONSTOR <ferdavsferdavs462@gmail.com>
2023-08-18 16:50:04 +0500

    git desktop1

--- Student.java
+++ Student.java
@@ -1,5 +1,7 @@
 public class Student {
     public static void main(String[] args) {
-
+       System.out.println("lopo");
+       System.out.println("Githup desktop");
+        System.out.println("git desktop");
     }
 }
