3cf04fb1fb4db7f443d370f65a57d3e9c214cff3
VICTUS <ferdavsferdavs462@gmail.com>
2023-08-18 15:28:57 +0500

    student class

--- main.java
+++ main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello java");
+        System.out.println("Github 3-dars");
     }
 }
